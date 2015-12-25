b365b2f6d138cafbd26022ec3f6d2b6edd33072d
mouse <mouse@Asus>
2015-12-25 14:50:52 +0200

    Pre-production 1.0.2

--- src/IuriiP/GoogleApi/GoogleApi.php
+++ src/IuriiP/GoogleApi/GoogleApi.php
@@ -60,17 +60,17 @@ class GoogleApi {
         }
         return [];
     }
-    
+
     /**
      * 
      * @param string $request - request type
      * @param array $param - request parameters
      * @return array
      */
-    public function call($request,$param) {
-        if(array_key_exists($request, $this->requestUrl)) {
+    public function call($request, $param) {
+        if (array_key_exists($request, $this->requestUrl)) {
             $param['key'] = $this->applicationKey;
-            $url = sprintf($this->requestUrl[$request],http_build_query($param));
+            $url = sprintf($this->requestUrl[$request], http_build_query($param));
             $answer = file_get_contents($url);
             $json = json_decode($answer, true);
 
@@ -80,13 +80,61 @@ class GoogleApi {
         }
         return [];
     }
-    
+
     public static function latlng($pair) {
-        return sprintf('%f,%f',$pair[0],$pair[1]);
+        return sprintf('%f,%f', $pair[0], $pair[1]);
     }
 
     public static function path($path) {
-        return implode('|',array_map(self::latlng,$path));
+        return implode('|', array_map(self::latlng, $path));
+    }
+
+    public static function getFirst($objects, $types = []) {
+        if ($types) {
+            foreach ($objects as $object) {
+                if (array_intersect($object['types'], $types)) {
+                    return $object;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static function getFirstPoint($objects) {
+        return self::getFirst($objects, [
+                    'street_address', // indicates a precise street address.
+                    'bus_station',
+                    'train_station',
+                    'transit_station', // indicate the location of a bus, train or public transit stop.
+                    'premise', // indicates a named location, usually a building or collection of buildings with a common name
+                    'subpremise', // indicates a first-order entity below a named location, usually a singular building within a collection of buildings with a common name
+                    'natural_feature', // indicates a prominent natural feature.
+                    'airport', // indicates an airport.
+                    'park', // indicates a named park.
+                    'point_of_interest', // indicates a named point of interest. Typically, these "POI"s are prominent local entities t
+        ]);
+    }
+
+    public static function getFirstLine($objects) {
+        return self::getFirst($objects, ['route']);
+    }
+    public static function getFirstRoute($objects) {
+        return self::getFirstLine($objects);
+    }
+    public static function getFirstArea($objects) {
+        return self::getFirst($objects, [
+                    'administrative_area_level_1', // indicates a first-order civil entity below the country level. Within the United States, these administrative levels are states. Not all nations exhibit these administrative levels.
+                    'administrative_area_level_2', // indicates a second-order civil entity below the country level. Within the United States, these administrative levels are counties. Not all nations exhibit these administrative levels.
+                    'administrative_area_level_3', // indicates a third-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+                    'administrative_area_level_4', // indicates a fourth-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+                    'administrative_area_level_5', // indicates a fifth-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+                    'colloquial_area', // indicates a commonly-used alternative name for the entity.
+                    'locality', // indicates an incorporated city or town political entity.
+                    'sublocality', // indicates a first-order civil entity below a locality. For some locations may receive one of the additional types: sublocality_level_1 to sublocality_level_5. Each sublocality level is a civil entity. Larger numbers indicate a smaller geographic area.
+                    'premise', // indicates a named location, usually a building or collection of buildings with a common name
+                    'natural_feature', // indicates a prominent natural feature.
+                    'park', // indicates a named park.
+        ]);
     }
 
 }
